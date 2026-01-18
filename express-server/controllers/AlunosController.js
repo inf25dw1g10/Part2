2fc5cfcccd41978494abc190725e268a5c1c19d9
IgorCruz7 <A043765@umaia.pt>
2026-01-18 01:53:42 +0000

    atual

--- express-server/controllers/AlunosController.js
+++ express-server/controllers/AlunosController.js
@@ -1,38 +1,30 @@
 /**
- * The AlunosController file is a very simple one, which does not need to be changed manually,
- * unless there's a case where business logic routes the request to an entity which is not
- * the service.
- * The heavy lifting of the Controller item is done in Request.js - that is where request
- * parameters are extracted and sent to the service, and where response is handled.
+ * Controller de Alunos — trata as rotas e delega para o service.
+ * Não precisa de lógica extra, apenas encaminha os pedidos.
  */
 
 const Controller = require('./Controller');
 const service = require('../services/AlunosService');
-const createAluno = async (request, response) => {
-  await Controller.handleRequest(request, response, service.createAluno);
-};
-
-const deleteAluno = async (request, response) => {
-  await Controller.handleRequest(request, response, service.deleteAluno);
-};
-
-const retrieveAluno = async (request, response) => {
-  await Controller.handleRequest(request, response, service.retrieveAluno);
-};
-
-const retrieveAlunos = async (request, response) => {
-  await Controller.handleRequest(request, response, service.retrieveAlunos);
-};
-
-const updateAluno = async (request, response) => {
-  await Controller.handleRequest(request, response, service.updateAluno);
-};
-
 
 module.exports = {
-  createAluno,
-  deleteAluno,
-  retrieveAluno,
-  retrieveAlunos,
-  updateAluno,
+  createAluno: async (req, res) => {
+    await Controller.handleRequest(req, res, service.createAluno);
+  },
+
+  deleteAluno: async (req, res) => {
+    await Controller.handleRequest(req, res, service.deleteAluno);
+  },
+
+  retrieveAluno: async (req, res) => {
+    await Controller.handleRequest(req, res, service.retrieveAluno);
+  },
+
+  retrieveAlunos: async (req, res) => {
+    await Controller.handleRequest(req, res, service.retrieveAlunos);
+  },
+
+  updateAluno: async (req, res) => {
+    await Controller.handleRequest(req, res, service.updateAluno);
+  }
 };
+
--- express-server/controllers/Controller.js
+++ express-server/controllers/Controller.js
@@ -7,10 +7,10 @@ const logger = require('../logger');
 class Controller {
   static sendResponse(response, payload) {
     /**
-    * The default response-code is 200. We want to allow to change that. in That case,
-    * payload will be an object consisting of a code and a payload. If not customized
-    * send 200 and the payload as received in this method.
-    */
+     * The default response-code is 200. We want to allow to change that. In that case,
+     * payload will be an object consisting of a code and a payload. If not customized
+     * send 200 and the payload as received in this method.
+     */
     response.status(payload.code || 200);
     const responsePayload = payload.payload !== undefined ? payload.payload : payload;
     if (responsePayload instanceof Object) {
@@ -30,27 +30,29 @@ class Controller {
   }
 
   /**
-  * Files have been uploaded to the directory defined by config.js as upload directory
-  * Files have a temporary name, that was saved as 'filename' of the file object that is
-  * referenced in request.files array.
-  * This method finds the file and changes it to the file name that was originally called
-  * when it was uploaded. To prevent files from being overwritten, a timestamp is added between
-  * the filename and its extension
-  * @param request
-  * @param fieldName
-  * @returns {string}
-  */
+   * Files have been uploaded to the directory defined by config.js as upload directory.
+   * Files have a temporary name, that was saved as 'filename' of the file object that is
+   * referenced in request.files array.
+   * This method finds the file and changes it to the file name that was originally called
+   * when it was uploaded. To prevent files from being overwritten, a timestamp is added between
+   * the filename and its extension.
+   * @param request
+   * @param fieldName
+   * @returns {string}
+   */
   static collectFile(request, fieldName) {
     let uploadedFileName = '';
     if (request.files && request.files.length > 0) {
-      const fileObject = request.files.find(file => file.fieldname === fieldName);
+      const fileObject = request.files.find((file) => file.fieldname === fieldName);
       if (fileObject) {
         const fileArray = fileObject.originalname.split('.');
         const extension = fileArray.pop();
         fileArray.push(`_${Date.now()}`);
         uploadedFileName = `${fileArray.join('')}.${extension}`;
-        fs.renameSync(path.join(config.FILE_UPLOAD_PATH, fileObject.filename),
-          path.join(config.FILE_UPLOAD_PATH, uploadedFileName));
+        fs.renameSync(
+          path.join(config.FILE_UPLOAD_PATH, fileObject.filename),
+          path.join(config.FILE_UPLOAD_PATH, uploadedFileName),
+        );
       }
     }
     return uploadedFileName;
@@ -61,43 +63,57 @@ class Controller {
     if (codeGenDefinedBodyName !== undefined) {
       return codeGenDefinedBodyName;
     }
-    const refObjectPath = request.openapi.schema.requestBody.content['application/json'].schema.$ref;
+    const refObjectPath =
+      request.openapi.schema.requestBody &&
+      request.openapi.schema.requestBody.content &&
+      request.openapi.schema.requestBody.content['application/json'] &&
+      request.openapi.schema.requestBody.content['application/json'].schema &&
+      request.openapi.schema.requestBody.content['application/json'].schema.$ref;
+
     if (refObjectPath !== undefined && refObjectPath.length > 0) {
-      return (refObjectPath.substr(refObjectPath.lastIndexOf('/') + 1));
+      return refObjectPath.substr(refObjectPath.lastIndexOf('/') + 1);
     }
     return 'body';
   }
 
   static collectRequestParams(request) {
     const requestParams = {};
-    if (request.openapi.schema.requestBody !== undefined) {
+
+    const method = request.method.toUpperCase();
+    const skipBody = method === 'GET' || method === 'DELETE';
+
+    // Body (apenas para POST/PUT/PATCH)
+    if (!skipBody && request.openapi.schema.requestBody !== undefined) {
       const { content } = request.openapi.schema.requestBody;
+
       if (content['application/json'] !== undefined) {
         const requestBodyName = camelCase(this.getRequestBodyName(request));
         requestParams[requestBodyName] = request.body;
       } else if (content['multipart/form-data'] !== undefined) {
-        Object.keys(content['multipart/form-data'].schema.properties).forEach(
-          (property) => {
-            const propertyObject = content['multipart/form-data'].schema.properties[property];
-            if (propertyObject.format !== undefined && propertyObject.format === 'binary') {
-              requestParams[property] = this.collectFile(request, property);
-            } else {
-              requestParams[property] = request.body[property];
-            }
-          },
-        );
+        Object.keys(content['multipart/form-data'].schema.properties).forEach((property) => {
+          const propertyObject = content['multipart/form-data'].schema.properties[property];
+          if (propertyObject.format !== undefined && propertyObject.format === 'binary') {
+            requestParams[property] = this.collectFile(request, property);
+          } else {
+            requestParams[property] = request.body[property];
+          }
+        });
       }
     }
 
-    request.openapi.schema.parameters.forEach((param) => {
-      if (param.in === 'path') {
-        requestParams[param.name] = request.openapi.pathParams[param.name];
-      } else if (param.in === 'query') {
-        requestParams[param.name] = request.query[param.name];
-      } else if (param.in === 'header') {
-        requestParams[param.name] = request.headers[param.name];
-      }
-    });
+    // Parâmetros de path, query e header (se existirem)
+    if (Array.isArray(request.openapi.schema.parameters)) {
+      request.openapi.schema.parameters.forEach((param) => {
+        if (param.in === 'path') {
+          requestParams[param.name] = request.openapi.pathParams[param.name];
+        } else if (param.in === 'query') {
+          requestParams[param.name] = request.query[param.name];
+        } else if (param.in === 'header') {
+          requestParams[param.name] = request.headers[param.name];
+        }
+      });
+    }
+
     return requestParams;
   }
 
